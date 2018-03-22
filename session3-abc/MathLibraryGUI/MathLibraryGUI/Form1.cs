80f5e0d70b2e4e521704d1b4bf3ca5da9914e6d9
David Lang <david.lang@etu.unice.fr>
2018-03-22 14:02:44 +0100

    session abc tp

---
+++ session3-abc/MathLibraryGUI/MathLibraryGUI/Form1.cs
@@ -0,0 +1,63 @@
+﻿using MathLibraryGUI.MathsOperationService;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathLibraryGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number1 = Int32.Parse(this.nombre1TextBox.Text);
+            int number2 = Int32.Parse(this.nombre2TextBox.Text);
+            MathsOperationsClient client1 = new MathsOperationsClient("Endpoint1");
+            MathsOperationsClient client2 = new MathsOperationsClient("Endpoint2");
+            MathsOperationsClient client3 = new MathsOperationsClient("Endpoint3");
+
+
+            this.resultEndpoint1Text.Text = client1.Add(number1, number2).ToString();
+            this.resultEndpoint2Text.Text = client2.Add(number1, number2).ToString();
+            this.resultEndpoint3Text.Text = client3.Add(number1, number2).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int number1 = Int32.Parse(this.nombre1TextBox.Text);
+            int number2 = Int32.Parse(this.nombre2TextBox.Text);
+            MathsOperationsClient client1 = new MathsOperationsClient("Endpoint1");
+            MathsOperationsClient client2 = new MathsOperationsClient("Endpoint2");
+            MathsOperationsClient client3 = new MathsOperationsClient("Endpoint3");
+
+
+            this.resultEndpoint1Text.Text = client1.Multiply(number1, number2).ToString();
+            this.resultEndpoint2Text.Text = client2.Multiply(number1, number2).ToString();
+            this.resultEndpoint3Text.Text = client3.Multiply(number1, number2).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int number1 = Int32.Parse(this.nombre1TextBox.Text);
+            int number2 = Int32.Parse(this.nombre2TextBox.Text);
+            MathsOperationsClient client1 = new MathsOperationsClient("Endpoint1");
+            MathsOperationsClient client2 = new MathsOperationsClient("Endpoint2");
+            MathsOperationsClient client3 = new MathsOperationsClient("Endpoint3");
+
+
+            this.resultEndpoint1Text.Text = client1.Divide(number1, number2).ToString();
+            this.resultEndpoint2Text.Text = client2.Divide(number1, number2).ToString();
+            this.resultEndpoint3Text.Text = client3.Divide(number1, number2).ToString();
+        }
+    }
+}
---
+++ session3-abc/MathsLibrary/MathsLibrary/MathsOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsLibrary
+{
+    public class MathsOperations : IMathsOperations
+    {
+        public int Add(int num1, int num2)
+        {
+            return (num1 + num2);
+        }
+
+        public int Divide(int num1, int num2)
+        {
+            int result;
+
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (Exception e)
+            {
+                CustomFaultDetails customFaultDetails = new CustomFaultDetails();
+                customFaultDetails.ErrorDetails = "division par 0";
+                customFaultDetails.ErrorID = 424242;
+                throw new FaultException<CustomFaultDetails>(customFaultDetails, "divide by 0");
+            }
+            return (result);
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return (num1 * num2);
+        }
+    }
+
+}
