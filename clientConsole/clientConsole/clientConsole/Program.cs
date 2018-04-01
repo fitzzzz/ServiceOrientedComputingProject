f8acdcbba4f51d3f53413f52760f809604767c0a
David Lang <david.lang@etu.unice.fr>
2018-04-01 21:04:40 +0200

    Google map sur le client graphique + implémentation du monitoring

---
+++ clientConsole/clientConsole/clientConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using clientConsole.VelibServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VelibServiceClient client = new VelibServiceClient();
+            client.get
+        }
+    }
+}
